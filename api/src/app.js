f0240f20a19cd2ab47d6e4a6a4897a80cd388bf1
murattt00 <muraturegen987@gmail.com>
2025-08-29 19:16:28 +0300

    updates for deploy

--- api/src/app.js
+++ api/src/app.js
@@ -22,11 +22,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/authors", authorRoute);
 app.use("/appointments", appointmentRoute);
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-});
+// Static file serving - Production için
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  });
+}
 
 //Server
 const port = process.env.PORT || 3000;
