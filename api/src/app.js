830a0a90c3ab2ff7be5a7adf2516655d64f8edf8
murattt00 <muraturegen987@gmail.com>
2025-08-29 17:52:41 +0300

    packetjson update

--- api/src/app.js
+++ api/src/app.js
@@ -21,6 +21,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/authors", authorRoute);
 app.use("/appointments", appointmentRoute);
 
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
+
 //Server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
